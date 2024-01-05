014fff25c31f625a8d48aa6ea35c5e208cedc350
Hongsugyeong <olivia258@naver.com>
2024-01-05 17:24:05 +0900

    [Test] 상품 기능 테스트

--- jpashop2/src/main/java/jpabook/jpashop2/domain/item/Item.java
+++ jpashop2/src/main/java/jpabook/jpashop2/domain/item/Item.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter @Setter
-public abstract class Item {
+public class Item {
 
     @Id @GeneratedValue
     @Column(name = "item_id")
---
+++ jpashop2/src/test/java/jpabook/service/ItemServiceTest.java
@@ -0,0 +1,38 @@
+package jpabook.service;
+
+import jpabook.jpashop2.Jpashop2Application;
+import jpabook.jpashop2.domain.item.Item;
+import jpabook.jpashop2.repository.ItemRepository;
+import jpabook.jpashop2.service.ItemService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Jpashop2Application.class)
+@Transactional
+public class ItemServiceTest {
+
+    @Autowired ItemService itemService;
+    @Autowired ItemRepository itemRepository;
+
+    @Test
+    public void 상품추가() throws Exception{
+        //given
+        Item item = new Item();
+        item.setName("자동차");
+
+        //when
+        itemService.saveItem(item);
+
+        //then
+        assertEquals(item,itemService.findOne(item.getId()));
+
+    }
+
+}
