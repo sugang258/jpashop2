4d0a736cd2225ced41bd29972f78fbf2bcc726d5
Hongsugyeong <olivia258@naver.com>
2024-01-06 13:26:40 +0900

    [Feat] 주문 개발 완료

--- jpashop2/src/main/java/jpabook/jpashop2/service/OrderService.java
+++ jpashop2/src/main/java/jpabook/jpashop2/service/OrderService.java
@@ -43,10 +43,23 @@ public class OrderService {
 
         //주문 저장
         orderRepository.saveOrder(order);
+
         return order.getId();
     }
 
-    //취소
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        //주문 취소
+        order.cancel();
+    }
 
-    //검색
+      //검색
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
 }
