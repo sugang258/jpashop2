c77a4d9f6b2cccd0eab783c6f5de912bee2928b7
Hongsugyeong <olivia258@naver.com>
2024-01-14 15:08:16 +0900

    [Feat] 주문 조회 API 1

--- jpashop2/src/main/java/jpabook/jpashop2/Jpashop2Application.java
+++ jpashop2/src/main/java/jpabook/jpashop2/Jpashop2Application.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop2;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Jpashop2Application {
@@ -10,5 +12,13 @@ public class Jpashop2Application {
 		SpringApplication.run(Jpashop2Application.class, args);
 
 	}
+
+	@Bean
+	Hibernate5JakartaModule hibernate5Module() {
+		Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+//		hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5JakartaModule;
+	}
+
 
 }
