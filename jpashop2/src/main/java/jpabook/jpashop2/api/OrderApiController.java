85737e1c651e0e55d826274143f4c72b573036d5
Hongsugyeong <olivia258@naver.com>
2024-01-16 23:53:15 +0900

    [Feat] 컬렉션 주문 조회 API 1

---
+++ jpashop2/src/main/java/jpabook/jpashop2/api/OrderApiController.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop2.api;
+
+import jpabook.jpashop2.domain.Order;
+import jpabook.jpashop2.domain.OrderItem;
+import jpabook.jpashop2.repository.OrderRepository;
+import jpabook.jpashop2.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
