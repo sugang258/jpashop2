f29f3834d910c370fa880dde2e7e23eaa81fc10d
Hongsugyeong <olivia258@naver.com>
2024-09-26 23:56:22 +0900

    [Test] Java 예외 처리 테스트

--- jpashop2/src/main/java/jpabook/jpashop2/controller/MemberController.java
+++ jpashop2/src/main/java/jpabook/jpashop2/controller/MemberController.java
@@ -9,14 +9,21 @@ import jpabook.jpashop2.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
 @Controller
 @RequiredArgsConstructor
@@ -79,4 +86,28 @@ public class MemberController {
         return "redirect:/";
 
     }
+
+    @PostMapping("/exception")
+    public void exceptionTest() throws Exception {
+        throw new Exception();
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Map<String, String>> ExceptionHandler(Exception e) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        log.info(e.getMessage());
+        log.info("Controller 내 ExceptionHandler 호출");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error type", httpStatus.getReasonPhrase());
+        map.put("code", "400");
+        map.put("message", "에러 발생");
+
+        return new ResponseEntity<>(map, responseHeaders, httpStatus);
+
+    }
+
+
 }
